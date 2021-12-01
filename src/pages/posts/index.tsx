ff802e44a3d1070ae92a6ef62a9786646a8222b2
Gabriel Leite <galeite@gmail.com>
2021-12-01 13:30:40 -0300

    finalizado projeto igNews

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,23 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import React from 'react';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
-export default function Posts() {
+type Post = {
+    slug: string,
+    title: string,
+    excerpt: string;
+    updatedAt: string;
+
+}
+interface PostsProps {
+    posts: Post[]
+}
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -13,21 +25,20 @@ export default function Posts() {
             </Head>
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>26 de novembro de 2021</time>
-                        <strong>Permanent access tokens</strong>
-                        <p>These tokens allow your application to permanently access this repository.</p>
-                    </a>
-                    <a href="#">
-                        <time>26 de novembro de 2021</time>
-                        <strong>Permanent access tokens</strong>
-                        <p>These tokens allow your application to permanently access this repository.</p>
-                    </a>
-                    <a href="#">
-                        <time>26 de novembro de 2021</time>
-                        <strong>Permanent access tokens</strong>
-                        <p>These tokens allow your application to permanently access this repository.</p>
-                    </a>
+                    {posts.map(post => (
+                        <Link
+                            key={post.slug}
+                            href={`/posts/${post.slug}`}>
+                            <a >
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
+
+
+                    ))}
+
                 </div>
             </main>
         </>
@@ -42,10 +53,21 @@ export const getStaticProps: GetStaticProps = async () => {
         fetch: ['post.title', 'post.content'],
         pageSize: 100
     })
-    console.log(JSON.stringify(response, null, 2))
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
     return {
         props: {
-
+            posts
         }
     }
 }
